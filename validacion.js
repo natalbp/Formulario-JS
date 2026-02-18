ad421d13a5431802c03d463ae8271de1b46294ca
Nat <nataliaberpino@gmail.com>
2026-02-18 07:00:32 -0500

    Ajustes de contraseña

--- validacion.js
+++ validacion.js
@@ -1,5 +1,7 @@
 let usuario = document.getElementById("usuario")
 let mensaje = document.getElementById("mensaje")
+let password = document.getElementById("password")
+
  usuario.addEventListener("input",function(evento){
    this.value = this.value.toLowerCase()
    if(/[^a-z]/g.test(this.value)){
@@ -8,13 +10,25 @@ let mensaje = document.getElementById("mensaje")
       this.borderColor = "2px solid"
    }
    else if (this.value){
-      mensaje.textContent = "usuario correcto"
+      mensaje.textContent = "Usuario correcto"
+      this.style.borderColor = "green"
+      this.borderColor = "2px solid"
    }
    else{
-      mensaje.textContent ="campo requerido"
-       this.style.borderColor = "green"
-       this.borderColor = "2px solid"
+      mensaje.textContent ="Campo requerido"
    }
    this.value = this.value.replace(/[^a-z]/g,"")
 
  })
+
+ password.addEventListener("input",function(evento){
+   if(this.value.length < 10){
+      mensaje2.textContent = "Contraseña debe tener al menos 10 caracteres"
+      this.style.borderColor = "red"
+      this.borderColor = "2px solid"
+   }else{
+      mensaje2.textContent = "Contraseña válida"
+      this.style.borderColor = "green"
+      this.borderColor = "2px solid"
+   }
+})
